e7b2b0462e19885ccd1041498812e38bdd1ebcfe
Anton Fedorov <avfedorov235@gmail.com>
2021-09-26 15:59:07 +0300

    +exceptions

--- exception/src/main/kotlin/ValidationException.kt
+++ exception/src/main/kotlin/ValidationException.kt
@@ -1,6 +1,24 @@
 class ValidationException(val errorCode: Array<ErrorCode>) : RuntimeException(errorCode.joinToString(",") { it.msg })
 
 enum class ErrorCode(val code: Int, val msg: String) {
-    INVALID_CHARACTER(100, "Недопустимый символ")
-    // ...
+    INVALID_CHARACTER(100, "Недопустимый символ"),
+    NULL_VALUE(101, "Null значение переменной"),
+
+    // PHONE
+    PHONE_NUMBER_LENGTH(200, "Номер телефона не равен 11 символам"),
+    PHONE_NUMBER_7_OR_8(201, "Номер телефона начинается не с 7 или 8"),
+
+    // NAME
+    NAME_IS_TOO_LONG(300, "Имя/Фамилия превысили 16 символов"),
+
+    // EMAIL
+    EMAIL_IS_TOO_LONG(400, "Длина email'а больше 32 символов"),
+    EMAIL_IS_TOO_SHORT(401, "Длина email'а меньше 5 символов"),
+    EMAIL_WRONG_FORMAT(402, "В Email не содержит символа '@' или содержит больше одного символа '@' "),
+
+    // SNILS
+    SNILS_LENGTH(500, "Код СНИЛСа слишком короткий/длинный"),
+    SNILS_SUMM_CHECK(501, "Указан неверный код СНИЛСа")
+
+
 }
--- exception/src/main/kotlin/Validator.kt
+++ exception/src/main/kotlin/Validator.kt
@@ -4,6 +4,86 @@ abstract class Validator<T> {
 
 class PhoneValidator : Validator<String>() {
     override fun validate(value: String?): List<ErrorCode> {
-        return listOf(ErrorCode.INVALID_CHARACTER)
+
+        if (value == null) return listOf(ErrorCode.NULL_VALUE)
+
+        val errors: MutableList<ErrorCode> = ArrayList()
+        if (value.length != 11)
+            return listOf(ErrorCode.PHONE_NUMBER_LENGTH)
+        if (!(value.first() == '7' || value.first() == '8'))
+            errors.add(ErrorCode.PHONE_NUMBER_7_OR_8)
+        if (!value.all { it in '0'..'9' })
+            errors.add(ErrorCode.INVALID_CHARACTER)
+
+        return errors
     }
 }
+
+class NameValidator : Validator<String>() {
+    override fun validate(value: String?): List<ErrorCode> {
+
+        if (value == null) return listOf(ErrorCode.NULL_VALUE)
+
+        if (value.length > 16)
+            return listOf(ErrorCode.NAME_IS_TOO_LONG)
+        if (!value.all { it in 'А'..'Я' || it in 'а'..'я'})
+            return listOf(ErrorCode.INVALID_CHARACTER)
+
+        return ArrayList()
+    }
+}
+
+class EmailValidator : Validator<String>() {
+    override fun validate(value: String?): List<ErrorCode> {
+
+        if (value == null) return listOf(ErrorCode.NULL_VALUE)
+
+        if (value.length < 5) {
+            return listOf(ErrorCode.EMAIL_IS_TOO_SHORT)
+        }
+        // Проверяем длину емейла перед проверкой на формат
+        if (value.length > 32) {
+            return listOf(ErrorCode.EMAIL_IS_TOO_LONG)
+        }
+
+        val emailFormat = "[A-z]+@[A-z]+\\.[A-z]+".toRegex()
+        if (!value.matches(emailFormat)) {
+            return listOf(ErrorCode.EMAIL_WRONG_FORMAT)
+        }
+
+        return ArrayList()
+    }
+}
+
+class SnilsValidator : Validator<String>() {
+    override fun validate(value: String?): List<ErrorCode> {
+
+        if (value == null) return listOf(ErrorCode.NULL_VALUE)
+
+        if (value.length != 11)
+            return listOf(ErrorCode.SNILS_LENGTH)
+        if (!value.all { it in '0'..'9' })
+            return listOf(ErrorCode.INVALID_CHARACTER)
+
+        var i = 9
+        val RANGE_FROM_0_TO_8 = IntRange(0, 8)
+        val RANGE_FROM_9_TO_10 = IntRange(9, 10)
+        val sum = value.substring(RANGE_FROM_0_TO_8)
+            .sumOf { Character.getNumericValue(it) * i-- }
+        val check = value.substring(RANGE_FROM_9_TO_10).toInt()
+
+        if (!compareSum(sum, check))
+            return listOf(ErrorCode.SNILS_SUMM_CHECK)
+
+        return ArrayList()
+    }
+
+    private fun compareSum(sum: Int, check: Int) : Boolean = when (sum) {
+        in 0..99 -> sum == check
+        in 100..101 -> 0 == check
+        else -> sum % 101 == check
+    }
+}
+
+
+
